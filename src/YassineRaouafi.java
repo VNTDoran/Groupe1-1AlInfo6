41b45ed8f665a83da38c630b96ce28e19e043af8
yassineraouafii <yassine.raouafi@esprit.tn>
2023-10-17 15:28:22 +0200

    fixed

--- src/YassineRaouafi.java
+++ src/YassineRaouafi.java
@@ -1,18 +1,4 @@
-import java.util.Scanner;
-
 public class YassineRaouafi {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("donner un nombre pour calculer le racine carre");
-        int n = sc.nextInt();
-        System.out.println("le racine carre de " + n + " est = " + Math.sqrt((double)n));
-        System.out.println("donner un nombre pour verifier nombre pair ou non");
-        int m = sc.nextInt();
-        if (m / 2 == 0) {
-            System.out.println("le nombre est pair");
-        } else {
-            System.out.println("le nombre est impair");
-        }
 
-    }
+    
 }
