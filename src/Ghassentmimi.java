e2f404e2db11fba693cb01cf8d13f3767afb9449
Mariem <2112001m@gmail.com>
2023-10-23 14:34:00 +0100

    conflict fixed

--- src/Ghassentmimi.java
+++ src/Ghassentmimi.java
@@ -9,7 +9,8 @@ public class Ghassentmimi {
         System.out.print("Entrez l'exposant : ");
         int exponent = scanner.nextInt();
         int[] tab = new int[0];
-        System.out.println("La puissance de " + base + " à laaa pduissance " + exponent + " est : " + calculPuiss(base,exponent));
+        System.out.println("/////////////////////////////////////////////");
+        System.out.println("La puissance de " + base + " à la puissance " + exponent + " est : " + calculPuiss(base,exponent));
         System.out.println("Le maximum est " + maximum(tab));
         System.out.print("Cause de conflit");
         System.out.print("Valorant");
